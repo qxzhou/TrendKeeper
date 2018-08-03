9e85c21cad52c504f610f6f05e00d8408469f689
qxzhou <zhou40469269@gmail.com>
2018-08-03 15:06:26 -0400

    Ffrmatting & bug fix

--- MySolution/spiders/mySolution_spider.py
+++ MySolution/spiders/mySolution_spider.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from datetime import datetime
+from scrapy.exceptions import CloseSpider
 from MySolution.items import MysolutionItem
 
 
 class MysolutionSpiderSpider(scrapy.Spider):
+
+    cur_time = datetime.now()
 
     name = 'mySolution_spider'
 
@@ -28,15 +32,18 @@ class MysolutionSpiderSpider(scrapy.Spider):
         item_list = response.xpath("//article[@class='press-release']")
         for i_item in item_list:
             mysolution_item = MysolutionItem()
-            mysolution_item["category"] = i_item.xpath("//strong[contains(text(),'Categories:')]/../a//text()").extract()
-            location = i_item.xpath("//strong[@class='date-line color-pr']//text()").extract_first()
+            category = i_item.xpath("//strong[contains(text(),'Categories:')]/../a//text()").extract_first()
+            i_category = " ".join(category.split())
+            mysolution_item["category"] = i_category
 
+
+            location = i_item.xpath("//strong[@class='date-line color-pr']//text()").extract_first()
             #generate the location list
             i_location = "".join(location.split())
             i_location = i_location.split(',')
             for i in range(2):
                 del i_location[-1]
-            # mysolution_item["location"] = ", ".join(i_location)
+            i_location = ", ".join(i_location)
             mysolution_item["location"] = i_location
 
             mysolution_item["keyword"] = i_item.xpath("//strong[contains(text(),'Tags:')]/../a//text()").extract_first()
@@ -48,9 +55,13 @@ class MysolutionSpiderSpider(scrapy.Spider):
             del i_date[0]
             #Change month from characters to numbers
             i_date[0] = month[i_date[0]]
-            #formatting day
+            #formatting date
             i_date[1] = i_date[1].strip(',')
+            i_date = "-".join(i_date)
             mysolution_item["date"] = i_date
+
+            # if i_date[:3] == ['8','2', '2018']:
+            #     raise CloseSpider('Termination Condition Met')
 
             yield mysolution_item
 
